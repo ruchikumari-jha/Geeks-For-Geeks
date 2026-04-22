2243d2cb2aaae16620a1d3ec4e935a93f892ae5e
Ruchi-Kumari <152387455+ruchikumari-jha@users.noreply.github.com>
2026-04-22 21:56:04 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Array Leaders/array-leaders.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> leaders(vector<int>& arr) {
+        int n = arr.size();
+        vector<int> result;
+
+        int maxRight = arr[n - 1];
+        result.push_back(maxRight);
+
+        // Traverse from right to left
+        for (int i = n - 2; i >= 0; i--) {
+            if (arr[i] >= maxRight) {
+                maxRight = arr[i];
+                result.push_back(arr[i]);
+            }
+        }
+
+        // Reverse result
+        reverse(result.begin(), result.end());
+
+        return result;
+    }
+};
